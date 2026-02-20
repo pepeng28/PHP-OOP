fa31665acd8aff021066ee3fb090b39f1a064ce2
pepeng28 <kangmunir28@gmail.com>
2026-02-20 21:14:59 +0700

    belajar bareng

--- data/Person.php
+++ data/Person.php
@@ -4,5 +4,9 @@ class Person {
   var string $name;
   var ?string $address = null;
   var string $country = "Indonesia";
+
+  function sayHello(string $name) {
+    echo "hallo $name" . PHP_EOL;
+  }
 }
 
